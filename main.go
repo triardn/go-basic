93c6ba78f726e461bbb7fcf1cb145360f094b624
Tri Ardini <tr.ardn@gmail.com>
2018-11-04 21:18:19 +0700

    basic loop

--- main.go
+++ main.go
@@ -5,19 +5,33 @@ import (
 )
 
 func main() {
-	stock := make(map[string]int)
+	// // basic loop
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
 
-	stock["milk"] = 4
-	stock["chees"] = 7
-	stock["mayonaise"] = 11
+	// // while loop
+	// x := 0
+	// for x < 7 {
+	// 	fmt.Println(x)
+	// 	x++
+	// }
 
-	fmt.Println(stock)
+	// // array range loop
+	// arr := []int{1, 2, 3}
 
-	stock["milk"] += 8
+	// for index, value := range arr {
+	// 	fmt.Println("index: ", index, "value: ", value)
+	// }
 
-	fmt.Println(stock)
+	// map loop
+	trashcan := make(map[string]int)
 
-	delete(stock, "mayonaise")
+	trashcan["leaf"] = 7
+	trashcan["paper"] = 10
+	trashcan["plastic"] = 3
 
-	fmt.Println(stock)
+	for key, value := range trashcan {
+		fmt.Println("key", key, "value", value)
+	}
 }
