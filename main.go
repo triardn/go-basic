a9be5b9f9413a1f8c91be708ba73f67028a08bd7
Tri Ardini <tr.ardn@gmail.com>
2018-11-04 19:57:32 +0700

    simple if else condition

--- main.go
+++ main.go
@@ -5,16 +5,12 @@ import (
 )
 
 func main() {
-	var arr [3]int
-	arr[1] = 4
-
-	// short hand fix length array
-	arr1 := [3]int{2, 3, 4}
-
-	// short hand dynamic length array
-	arr2 := []int{4, 5, 6}
-	arr2 = append(arr2, 7)
-
-	fmt.Println(arr1)
-	fmt.Println(arr2)
+	a := 7
+	if a < 4 {
+		fmt.Println("Kurang dari 4")
+	} else if a == 4 {
+		fmt.Println("Sama dengan 4")
+	} else {
+		fmt.Println("Lebih dari 4")
+	}
 }
