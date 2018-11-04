450a25106c01558a5f607c0ea9b4fea5595a7888
Tri Ardini <tr.ardn@gmail.com>
2018-11-04 22:02:02 +0700

    pointer

--- main.go
+++ main.go
@@ -4,28 +4,13 @@ import (
 	"fmt"
 )
 
-type Person struct {
-	Name string
-	Age  int
+func main() {
+	i := 10
+	inc(&i)
+
+	fmt.Println(i)
 }
 
-func main() {
-	p := Person{Name: "Tri", Age: 26}
-
-	fmt.Println(p.Name)
-
-	// Array of struct
-	arrp := []Person{
-		{Name: "Tri", Age: 26},
-		{Name: "Tristan", Age: 3},
-	}
-
-	fmt.Println(arrp[1].Name)
-
-	// Traverse over array of struct
-	for _, elem := range arrp {
-		fmt.Println(elem.Name)
-		fmt.Println(elem.Age)
-		fmt.Println("")
-	}
+func inc(x *int) {
+	*x++
 }
