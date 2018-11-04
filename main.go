31060c6d09e435e1af191cbf46c737584f1a948a
Tri Ardini <tr.ardn@gmail.com>
2018-11-04 19:51:41 +0700

    basic array

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 )
 
 func main() {
-	var a int = 12
-	var b int = 24
-	var c float64 = 7.3
-	var name string = "Naruto "
-	var enemy string = "Sasuke"
-	fmt.Println("horaay")
-	fmt.Println(a + b)
-	fmt.Println(c)
-	fmt.Println(name + enemy)
+	var arr [3]int
+	arr[1] = 4
+
+	// short hand fix length array
+	arr1 := [3]int{2, 3, 4}
+
+	// short hand dynamic length array
+	arr2 := []int{4, 5, 6}
+	arr2 = append(arr2, 7)
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
 }
